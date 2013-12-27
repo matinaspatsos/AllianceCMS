f8df83711b8ad5af191ce6a2b2ff037a558110ef
jburns131 <jburns131@gmail.com>
2013-12-26 19:21:05 -0500

    Incremental commit

--- public_html/themes/Gumbo/theme.tpl.php
+++ public_html/themes/Gumbo/theme.tpl.php
@@ -10,53 +10,169 @@
 <html class="no-js" lang="en" itemscope itemtype="http://schema.org/Product"> <!--<![endif]-->
 	<head>
 		<meta charset="utf-8">
-		
+
 		<!-- Use the .htaccess and remove these lines to avoid edge case issues.
 		More info: h5bp.com/b/378 -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		
+
 		<title><?php echo $venue_title; ?></title>
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
 		<meta name="author" content="humans.txt">
-		
+
 		<link rel="shortcut icon" href="<?php echo $theme_folder; ?>/favicon.ico" type="image/x-icon" />
-		
+
 		<!-- Facebook Metadata /-->
 		<meta property="fb:page_id" content="" />
 		<meta property="og:image" content="" />
 		<meta property="og:description" content=""/>
 		<meta property="og:title" content=""/>
-		
+
 		<!-- Google+ Metadata /-->
 		<meta itemprop="name" content="">
 		<meta itemprop="description" content="">
 		<meta itemprop="image" content="">
-		
+
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-	
+
 		<!-- We highly recommend you use SASS and write your custom styles in sass/_custom.scss.
 		However, there is a blank style.css in the css directory should you prefer -->
 		<link rel="stylesheet" href="<?php echo $theme_folder; ?>/css/gumby.css">
 		<!-- <link rel="stylesheet" href="css/style.css"> -->
-	
+
 		<script src="<?php echo $theme_folder; ?>/js/libs/modernizr-2.6.2.min.js"></script>
 	</head>
 
 	<body>
 
+	    <nav class="navcontain">
+            <div id="nav" class="navbar" gumby-fixed="top">
+                <div class="row">
+                    <a class="toggle" href="#" gumby-trigger="nav > .row > ul">
+                        <i class="icon-menu"></i>
+                    </a>
+                    <h1 class="four columns logo">
+                        <a href="#">
+                            <img gumby-retina="" src="img/gumby_mainlogo.png"></img>
+                        </a>
+                    </h1>
+                    <ul class="eight columns">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Features</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li class="field"></li>
+                    </ul>
+                </div>
+            </div>
+	    </nav>
+
+	    <!--
+		<nav class="row navbar pretty" id="nav1">
+		-->
+            <!-- Toggle for mobile navigation, targeting the <ul> -->
+
+	    <!--
+            <a class="toggle" gumby-trigger="#nav1 > ul" href="#"><i class="icon-menu"></i></a>
+
+            <h1 class="four columns logo">
+                <a href="#">
+                    <img src="/img/gumby_mainlogo.png" gumby-retina />
+                </a>
+            </h1>
+
+            <ul class="eight columns">
+                <li><a href="#">Features</a></li>
+                <li>
+                    <a href="#">Documentation</a>
+                    <div class="dropdown">
+                        <ul>
+                            <li><a href="#">The Grid</a></li>
+                            <li><a href="#">UI Kit</a></li>
+                            <li><a href="#">Sass</a></li>
+                            <li><a href="#">JavaScript</a></li>
+                            <li><a href="#">Demo</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li><a href="#">Customize</a></li>
+            </ul>
+        </nav>
+        -->
+
+        <p>
+            Lorem ipsum dolor sit amet, sit esse oblique facilisis cu, ius eu eirmod iisque indoctum. Vim eu aeterno deserunt prodesset, agam electram cum eu, at qui veniam signiferumque. Ad eam congue quidam, usu quot eirmod id, usu omnes perfecto vulputate et. Tamquam persius alterum ad mei, esse signiferumque nam ei, at nam incorrupte consectetuer.
+        </p>
+        <p>
+            Te pro eruditi debitis nusquam, eam ne solet tation. Ea esse oblique quo, at alterum facilisi qui. Omnium tibique dignissim id mea, pro at amet duis quodsi. Nam in dicant suscipit consectetuer, ei vitae consulatu nec. Ius quot dicit no, quo unum dictas explicari eu.
+        </p>
+        <p>
+            Ex semper omittantur reprehendunt pri, et nisl iusto delenit sea. Stet iracundia vis id. Te duo alia bonorum. Iisque accusata efficiendi in pri, cu per causae aliquando, eum eros molestie accommodare ei. Sensibus dignissim maiestatis ius in. His eleifend deseruisse neglegentur ex, postea corrumpit ius ad. Iusto copiosae moderatius sea in.
+        </p>
+        <p>
+            Te fabulas deleniti his, ut salutandi iracundia mel, ei pro natum abhorreant. Aeque aeterno veritus vim cu, affert graecis mel at. Nam ad quod omnes nostrum, his cu inani lucilius accusata. Aeque malorum percipitur ad sea.
+        </p>
+        <p>
+            His et quem ornatus electram, vel cu modus deserunt. Eirmod singulis sea ex. An nisl elit eruditi sed, et mel quem mazim. Nam ne erat facer, oratio salutandi intellegebat pro no. Has omittam temporibus ea. Posse quidam gubergren an quo, placerat reformidans complectitur ex ius. Aperiam maiestatis vis ei.
+        </p>
+        <p>
+            At tantas labitur epicurei pri. Ut atqui feugait deserunt qui, alienum propriae ex sed, mei cu assum aliquip. Qui reque doctus ne, ne mei error reprehendunt. Mei tation mandamus rationibus in, alterum docendi duo in. Sea id officiis liberavisse, putent interpretaris vim at.
+        </p>
+        <p>
+            Ne qui euismod urbanitas consectetuer, omnis omnesque fabellas his ne. Civibus postulant per at. His ne probo mutat cotidieque. Brute urbanitas sed in. Soluta voluptatibus mei id, ancillae temporibus interpretaris per in, nam erant similique in. Per explicari consetetur id, usu ex dolor constituam reprehendunt, et enim vocibus constituto ius.
+        </p>
+        <p>
+            Id pri molestie accommodare. Duo et aperiri adolescens, quando nominavi nam ut. At posse aeterno lobortis eam, dolorem perpetua incorrupte sea in. Omnium theophrastus eum et, vim aliquip feugiat assentior cu. Pri case commodo constituam an, pro cu utroque adipisci. Ut audire lobortis his, et cum invidunt quaestio, et legere admodum omnesque per.
+        </p>
+        <p>
+            His no mucius putent, assum aperiri alterum ne vim, dico nobis omittantur his in. Vim invidunt inimicus ad, laudem eripuit contentiones eum no, ea detraxit salutatus eam. Fugit dolorum definiebas nec ex, amet corpora vel ne, ei nam labores reprimique reformidans. Option epicurei adolescens per an.
+        </p>
+        <p>
+            An fierent iracundia quo, suscipit adipisci ad vel. Nec aliquip quaerendum at. Mea ad iuvaret accusam repudiandae. Viderer definitionem cu vel. Putant debitis probatus vim ne, ad mei dico praesent. Vel no dissentiet conclusionemque.
+        </p>
+        <p>
+            Decore verear rationibus te mea, ei has velit fierent efficiendi. Eos et nullam laboramus vituperatoribus, soluta euismod omittam cu vis, sint impetus definitionem pro ad. In vide cetero nec. Mentitum euripidis adversarium vis ei. Laudem perfecto singulis pri et. Et has tation nostro iriure.
+        </p>
+        <p>
+            Pro graecis verterem in, ne laudem signiferumque quo. Usu ut labitur discere, cu deseruisse vituperata eum. Et vim quaestio recteque disputationi. Quo no vidit scribentur.
+        </p>
+        <p>
+            Quo appareat torquatos philosophia id, qui noster comprehensam an. Duo ex diceret iudicabit laboramus, ius at debet quidam. Quo vide solum paulo at. Eu summo eruditi eum, pri in vivendum torquatos. Vis impedit temporibus in, vis ei tale viderer ancillae. Ad errem iracundia deseruisse eum.
+        </p>
+        <p>
+            Novum rationibus nam at, ne sit facer temporibus, ferri impedit adolescens no sit. Eam eius graeci temporibus te, at vis aperiri voluptua voluptatum. Usu admodum commune scripserit an. Everti invenire at eam, cum cu ubique euismod dolores, quis facilisis pri te.
+        </p>
+        <p>
+            Mel eu summo tacimates pertinax. Enim homero liberavisse cu mel, duo ea nihil verterem. Vel an saperet facilisis, eu cum ullum corpora necessitatibus. At ius falli ancillae verterem, utinam dicunt delicata ea vix. Invidunt recteque aliquando an est, consul rationibus duo in.
+        </p>
+        <p>
+            Quaeque laboramus consequuntur no cum, graeco facilisis an vel. Ei wisi errem audire cum. In usu omnis modus debet, prompta splendide ut vis. Ex paulo singulis platonem eos, sea cu vidit noster voluptatibus. Has cu dolore delenit reprehendunt, nam ne modo fabulas facilisis. Quo ut libris everti.
+        </p>
+        <p>
+            Ullum commodo epicuri cu has. An impedit fuisset vis, mei no brute zril mediocrem. Latine intellegam mei eu, quo at etiam soluta. Dicat graece te mei. Alia ornatus at eum, unum mucius conceptam sea et, ne possit consectetuer usu.
+        </p>
+        <p>
+            Saepe intellegat per at, ad eam persius maluisset intellegat. Ei his graeco maiorum concludaturque, sea option regione ea. Scaevola torquatos mea ea. Vix libris deseruisse an. Vim ad postea utroque, eu vis saperet commune menandri, has an veniam diceret reprimique.
+        </p>
+        <p>
+            Ex mea veniam apeirian forensibus, sed cetero maiorum recteque eu. Mea id eros reque intellegebat, ius ad tollit nostro neglegentur. Etiam appetere in duo. At est dolor delicata, ad eum paulo assueverit. Qui an summo homero.
+        </p>
+        <p>
+            Sit enim tibique eu, an vis omnis liber invidunt. Duo soleat senserit ei, eum regione fierent oporteat id. At omnium corpora vix, ut paulo erroribus vis, ei aliquam ocurreret cum. Ei nam malorum omittam mandamus, cu idque utamur vis. Ne vim nemore nostrud.
+        </p>
+
 		<!-- Grab Google CDN's jQuery, fall back to local if offline -->
 		<!-- 2.0 for modern browsers, 1.10 for .oldie -->
 		<script>
 			var oldieCheck = Boolean(document.getElementsByTagName('html')[0].className.match(/\soldie\s/g));
-			
+
 			if(!oldieCheck) {
 				document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"><\/script>');
 			} else {
 				document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"><\/script>');
 			}
 		</script>
-		
+
 		<script>
 		if(!window.jQuery) {
 			if(!oldieCheck) {
@@ -66,7 +182,7 @@
 			}
 		}
 		</script>
-	
+
 		<!--
 		Include gumby.js followed by UI modules followed by gumby.init.js
 		Or concatenate and minify into a single file -->
@@ -81,33 +197,33 @@
 		<script src="<?php echo $theme_folder; ?>/js/libs/ui/gumby.navbar.js"></script>
 		<script src="<?php echo $theme_folder; ?>/js/libs/ui/jquery.validation.js"></script>
 		<script src="<?php echo $theme_folder; ?>/js/libs/gumby.init.js"></script>
-	
+
 		<!--
 		Google's recommended deferred loading of JS
 		gumby.min.js contains gumby.js, all UI modules and gumby.init.js
-	
+
 		Note: If you opt to use this method of defered loading,
 		ensure that any javascript essential to the initial
 		display of the page is included separately in a normal
 		script tag.
-	
+
 		<script type="text/javascript">
 			function downloadJSAtOnload() {
 				var element = document.createElement("script");
-				element.src = "<?php echo $theme_folder; ?>/js/libs/gumby.min.js";
+				element.src = "<?php /*echo $theme_folder;//*/ ?>/js/libs/gumby.min.js";
 				document.body.appendChild(element);
 			}
-			
+
 			if (window.addEventListener)
 				window.addEventListener("load", downloadJSAtOnload, false);
 			else if (window.attachEvent)
 				window.attachEvent("onload", downloadJSAtOnload);
 			else window.onload = downloadJSAtOnload;
 		</script> -->
-	
+
 			<script src="<?php echo $theme_folder; ?>/js/plugins.js"></script>
 			<script src="<?php echo $theme_folder; ?>/js/main.js"></script>
-	
+
 		<!-- Change UA-XXXXX-X to be your site's ID -->
 		<!--<script>
 			window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
@@ -115,7 +231,7 @@
 			  load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
 			});
 		</script>-->
-	
+
 		<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
 		chromium.org/developers/how-tos/chrome-frame-getting-started -->
 		<!--[if lt IE 7 ]>
